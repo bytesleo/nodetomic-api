60b84e75078883e4e7507dc98aacd90f512a0860
Leonardo Rico <leonardo.ricog@gmail.com>
2017-04-14 21:21:43 -0500

    update babel

--- gulpfile.js
+++ gulpfile.js
@@ -3,22 +3,31 @@
 *@Github: https://github.com/kevoj
 */
 
-var gulp = require('gulp'),
+const gulp = require('gulp'),
     rename = require('gulp-rename'),
     clean = require('gulp-rimraf'),
     color = require('gulp-color'),
     replace_string = require('gulp-replace'),
     runSequence = require('run-sequence'),
-    jsonModify = require('gulp-json-modify');
+    jsonModify = require('gulp-json-modify'),
+    babel = require('gulp-babel');
 
-var dist = './dist';
-var dist_server = dist + '/server';
+const dist = './../dist';
+const dist_server = dist + '/server';
 
-gulp.task('serve:build:clean', function() {
-    return gulp.src([dist + "/server/*"], {read: false}).pipe(clean({force: true}));
+gulp.task('build', function() {
+    runSequence('build-clean', 'build-babel', 'build-replace');
 });
 
-gulp.task('serve:build:replace', function() {
+gulp.task('build-clean', function() {
+    return gulp.src([dist_server + "/*"], {read: false}).pipe(clean({force: true}));
+});
+
+gulp.task('build-babel', function() {
+    return gulp.src('server/**/*.js').pipe(babel()).pipe(gulp.dest(dist_server));
+});
+
+gulp.task('build-replace', function() {
     gulp.src([dist_server + "/config/production.js"]).pipe(rename('index.js')).pipe(gulp.dest(dist_server + '/config'));
     gulp.src([
         dist_server + "/config/production.js",
