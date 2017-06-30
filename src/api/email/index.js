1fa227f18ca04f86a222709a2a81452dce991634
Leonardo Rico <leonardo.ricog@gmail.com>
2017-06-30 15:22:39 -0500

    updated paths

--- src/api/email/index.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import * as controller from './email.controller';
-
-const router = express.Router();
-
-router.get('/', controller.index);
-router.get('/v/:folder/:name', controller.preview);
-
-export default router;
