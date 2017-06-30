82b1740a4934492bd43f7e7b0e91b6a3986467b9
Leonardo Rico <leonardo.ricog@gmail.com>
2017-06-30 15:22:48 -0500

    updated paths

--- src/api/file/index.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import * as controller from './file.controller';
-
-const router = express.Router();
-
-router.get('/upload', controller.upload);
-
-export default router;
