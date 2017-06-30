d1e375457b351eacaa312fae39f3c8a747999844
Leonardo Rico <leonardo.ricog@gmail.com>
2017-06-30 15:26:05 -0500

    refactor routers

--- src/core/router.js
+++ src/core/router.js
@@ -3,15 +3,17 @@ import config from '../config';
 
 export default (app) => {
 
-  //-> api/example: push in noLoad 'example' or renamed api/_example
-  fs.readdirSync(`${config.base}/api`).forEach(route => {
-    if (config.router.ignore.indexOf(route) < 0)
-      if (route.charAt(0) !== '_')
-        app.use(`/api/${route}`, require(`../api/${route}`).default);
-  });
+  //-> api/version/example: push in noLoad 'example' or renamed api/version/_example
+  fs.readdirSync(`${config.base}/api`).forEach(version => {
+    fs.readdirSync(`${config.base}/api/${version}`).forEach(route => {
+      if (config.router.ignore.indexOf(route) < 0)
+        if (route.charAt(0) !== '_')
+          app.use(`/api/${version}/${route}`, require(`../api/${version}/${route}`).default);
+    });
+ });
 
   // Routers Manual
   app.use('/auth', require('../lib/auth').default);
-  // app.use('/api/hello', require('../api/hello'));
+  // app.use('/api/version/hello', require('../api/version/hello'));
 
 };
