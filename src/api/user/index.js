de2b64f0fc3c38665c5a6673febb2a02945b3d87
Leonardo Rico <leonardo.ricog@gmail.com>
2017-06-30 11:49:08 -0500

    added admin controller

--- src/api/user/index.js
+++ src/api/user/index.js
@@ -1,15 +1,16 @@
 import express from 'express';
 import * as auth from '../../lib/auth/middleware';
 import * as controller from './user.controller';
+import * as controllerAdmin from './user.controller.admin';
 
 const router = express.Router();
 
-router.get('/', auth.isAuthenticated(['admin']), controller.list);
+router.get('/', auth.isAuthenticated(['admin']), controllerAdmin.list);
 router.get('/me', auth.isAuthenticated(), controller.me);
 router.get('/:id', auth.isAuthenticated(), controller.read);
-router.delete('/:id', auth.isAuthenticated(['admin']), controller.remove);
 router.post('/', controller.create);
 router.put('/', auth.isAuthenticated(), controller.update);
-router.put('/:id', auth.isAuthenticated(['admin']), controller.updateByAdmin);
+router.put('/:id', auth.isAuthenticated(['admin']), controllerAdmin.update);
+router.delete('/:id', auth.isAuthenticated(['admin']), controllerAdmin.remove);
 
 export default router;
--- src/api/user/user.controller.js
+++ src/api/user/user.controller.js
@@ -1,17 +1,5 @@
 import User from './user.model';
 
-export function list(req, res) {
-
-  User.find({}, {
-    password: 0
-  }).exec().then(users => {
-    res.json(users)
-  }).catch(err => res.status(500).json({
-    error: err
-  }));
-
-}
-
 export function read(req, res) {
 
   User.findById(req.params.id, {
@@ -66,50 +54,6 @@ export function update(req, res) {
       });
     }
 
-  }).catch(err => res.status(500).json({
-    error: err
-  }));
-
-}
-
-export function updateByAdmin(req, res) {
-
-  User.findById(req.params.id).then(user => {
-
-    try {
-      user.username = req.body.username;
-      user.email = req.body.email;
-      user.name = req.body.name;
-      user.lastname = req.body.lastname;
-      if (req.body.password)
-        user.password = req.body.password;
-      user.roles = req.body.roles || [];
-      user.status = req.body.status;
-
-      user.save().then(result =>
-        res.json({
-          update: true
-        })).catch(err => res.status(500).json({
-        error: err
-      }));
-
-    } catch (err) {
-      res.status(500).json({
-        error: err
-      });
-    }
-
-  }).catch(err => res.status(500).json({
-    error: err
-  }));
-}
-
-export function remove(req, res) {
-
-  User.deleteOne({
-    _id: req.params.id
-  }).then(result => {
-    res.json(result);
   }).catch(err => res.status(500).json({
     error: err
   }));
