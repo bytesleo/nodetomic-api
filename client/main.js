bf8ec5138decc8579e58f08b1a5d7c22e5d49f76
Leonardo Rico <leonardo.ricog@gmail.com>
2017-06-30 15:21:55 -0500

    updated js to client default

--- client/main.js
+++ client/main.js
@@ -2,32 +2,7 @@ var host = location.origin;
 new Vue({
   el: '#nodetomicApp',
   data: {
-    greets: [],
-    username: '',
-    password: '',
-    token: '',
-    version: 'v1.9.5'
-  },
-  created: function() {
-    this.fetchData()
-  },
-  methods: {
-    fetchData: function() {
-      var self = this;
-      $.get(host + "/api/hello/", function(data) {
-        self.greets = data;
-      });
-    },
-    login: function(event) {
-      var self = this;
-      var user = {
-        username: this.username,
-        password: this.password
-      }
-      $.post(host + "/auth/local", user, function(data) {
-        if (data.token)
-          self.token = data.token;
-      });
-    }
+    project: 'Nodetomic API',
+    version: 'v1.9.37'
   }
 });
