4b41466f32efde3f640af46172df733d505caf70
Leonardo Rico <leonardo.ricog@gmail.com>
2017-06-30 21:08:22 -0500

    updated status

--- src/core/mongoose/status.js
+++ src/core/mongoose/status.js
@@ -1,51 +1,26 @@
-import config from '../../config';
 import colors from 'colors';
 
-export default(db, conf) => {
+export default(db, config) => {
 
   // When successfully connected
   db.on('connected', (err) => {
-    console.log(`MongoDB-> connected on ${conf.uri}`.bgGreen);
+    console.log(`MongoDB-> connected on ${config.database.mongo.db.uri}`.bgGreen);
   });
 
   db.once('open', () => {
-    // Seed
-    db.db.listCollections().toArray((err, names) => {
-      if (err)
-        console.log(err);
-      conf.seeds.forEach(seed => {
-        switch (seed.seed) {
-          case 'once':
-            let found;
-            names.forEach(elem => {
-              if (`${seed.model.toLowerCase()}s` == elem.name) {
-                found = true;
-              }
-            });
-            if (!found)
-              require(`${config.base}${seed.path}`);
-            break;
-          case 'alway':
-            require(`${config.base}${seed.path}`);
-            break;
-          case 'never':
-            console.log('No seeds were sown :)');
-            break;
-          default:
-        }
-      });
-    });
+    // Plant seed
+    require('./seed').default(db, config);
   });
 
   // If the connection throws an error
   db.on('error',err => {
-      console.log(`MongoDB-> connection error: ${conf.uri} details->${err}`.bgRed);
+      console.log(`MongoDB-> connection error: ${config.database.mongo.db.uri} details->${err}`.bgRed);
       process.exit(-1);
   });
 
   // When the connection is disconnected
   db.on('disconnected',err => {
-      console.log(`MongoDB-> disconnected: ${conf.uri}`.bgRed);
+      console.log(`MongoDB-> disconnected: ${config.database.mongo.db.uri}`.bgRed);
   });
 
 };
