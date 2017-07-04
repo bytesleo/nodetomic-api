8bb6f5df783f087598f882274a906f5e2ff441ee
Leonardo Rico <leonardo.ricog@gmail.com>
2017-07-03 19:23:17 -0500

    updated 1.9.71

--- client/main.js
+++ client/main.js
@@ -2,7 +2,7 @@ var host = location.origin;
 new Vue({
   el: '#nodetomicApp',
   data: {
-    project: 'Nodetomic API',
-    version: 'v1.9.37'
+    project: 'API',
+    version: 'v1.9.71'
   }
 });
